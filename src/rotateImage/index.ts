76b36b930456045587b041808d46abc5650b90f3
Craig Forrest <craforrest97@gmail.com>
2021-08-22 13:12:01 +0100

    lint

--- src/rotateImage/index.ts
+++ src/rotateImage/index.ts
@@ -1,8 +1,10 @@
+/* eslint-disable no-param-reassign */
+
 export const rotateImage = (image: number[][]): number[][] => {
   const size = image.length;
 
-  for (let y = 0; y < size / 2; y++) {
-    for (let x = 0; x < (size - 1) / 2; x++) {
+  for (let y = 0; y < size / 2; y += 1) {
+    for (let x = 0; x < (size - 1) / 2; x += 1) {
       const pos1 = { x, y };
       const pos2 = { x: size - y - 1, y: x };
       const pos3 = { x: size - x - 1, y: size - y - 1 };
